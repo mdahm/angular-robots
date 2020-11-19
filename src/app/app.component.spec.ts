634b6dace2e4b6f37410327882ccdcef5ee4f55c
Markus Dahm <markus.dahm@akquinet.de>
2020-11-19 10:28:01 +0100

    tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {GridComponent} from "./grid/grid.component";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,12 +10,12 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   });
-  //
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
   //
   // it(`should have as title 'angular'`, () => {
   //   const fixture = TestBed.createComponent(AppComponent);
